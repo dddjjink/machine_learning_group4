98be46b6115fcf9283c45b4d8b2f09eae35723a7
Hydrogen-577 <97822083+Hydrogen-577@users.noreply.github.com>
2023-07-16 22:01:52 +0800

    Update RMSE.py

--- code/evaluation/RMSE.py
+++ code/evaluation/RMSE.py
@@ -1,2 +1,13 @@
 import Evaluation
 
+
+# 均方根误差
+class RMSE(Evaluation):
+    def __str__(self):
+        return 'MSE'
+
+    def __call__(self, y, y_pred):
+        return self.loss(y, y_pred)
+
+    def loss(self, y, y_pred):
+        return math.sqrt(0.5 * np.sum((y_pred - y) ** 2, axis=-1))
