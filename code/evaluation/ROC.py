23e760e65701f1ea5a887c686d6285732f3b87f2
Hydrogen-577 <97822083+Hydrogen-577@users.noreply.github.com>
2023-07-17 17:08:33 +0800

    Update ROC.py

--- code/evaluation/ROC.py
+++ code/evaluation/ROC.py
@@ -1,16 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import Evaluation
+from Evaluation import Evaluation
 
 
 # ROC曲线，可视化
 class ROC(Evaluation):
-    def __init__(self, y_true, y_pred):
+    def __init__(self, y_test, y_pred):
+        super().__init__(y_test, y_pred)
         self.tpr = []
         self.fpr = []
-        self.y_true = y_true
-        self.y_pred = y_pred
-    def ROCCurve(self):
+
+    def __call__(self, *args, **kwargs):
+        self.plot()
+
+    def roc_curve(self):
         true_positives = 0
         false_positives = 0
         total_positives = np.sum(self.y_true)
@@ -28,10 +31,64 @@ class ROC(Evaluation):
             self.tpr.append(tpr)
             self.fpr.append(fpr)
         return self.tpr, self.fpr
+
     def plot(self):
-        tpr, fpr = self.ROCCurve()
+        tpr, fpr = self.roc_curve()
         plt.plot(fpr, tpr)
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
         plt.title('ROC Curve')
         plt.show()
+
+
+# # ROC曲线示例用法
+# if __name__ == '__main__':
+#     import pandas as pd
+#     from sklearn.model_selection import train_test_split
+#     from sklearn.linear_model import LogisticRegression
+# 
+#     '''
+#     对本例的鸢尾花数据集不适用，ROC曲线适用二分类问题
+#     '''
+#     # 鸢尾花数据集
+#     # 数据载入
+#     iris = pd.read_csv('../data/Iris.csv')
+#     # print(iris.head(10))
+#     # 数据分割
+#     x = iris[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+#     y = iris['Species'].values
+#     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # print(x_train, x_test, y_train, y_test)
+#     # 模型训练、预测
+#     clf = LogisticRegression(random_state=0, solver='lbfgs')
+#     clf.fit(x_train, y_train)
+#     train_predict = clf.predict(x_train)
+#     test_predict = clf.predict(x_test)
+#     # 模型评估
+#     ROC_train = ROC(y_train, train_predict)
+#     ROC_test = ROC(y_test, test_predict)
+#     ROC_train()
+#     ROC_test()
+# 
+#     '''
+#     对本例的红酒数据集不适用，ROC曲线适用二分类问题
+#     '''
+#     # 红酒数据集
+#     # 数据载入
+#     wine = pd.read_csv('../data/WineQT.csv')
+#     # print(wine.head(10))
+#     # 数据分割
+#     x = wine.drop(['quality', 'Id'], axis=1).values
+#     y = wine['quality'].values
+#     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # print(x_train, x_test, y_train, y_test)
+#     # 模型训练、预测
+#     clf = LogisticRegression(random_state=0, solver='lbfgs')
+#     clf.fit(x_train, y_train)
+#     train_predict = clf.predict(x_train)
+#     test_predict = clf.predict(x_test)
+#     # 模型评估
+#     ROC_train = ROC(y_train, train_predict)
+#     ROC_test = ROC(y_test, test_predict)
+#     ROC_train()
+#     ROC_test()
