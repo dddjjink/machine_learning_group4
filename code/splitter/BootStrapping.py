2bd04b92eb95dc4e5d22e1617473f90ab0498b80
Hydrogen-577 <97822083+Hydrogen-577@users.noreply.github.com>
2023-07-17 15:40:24 +0800

    Update BootStrapping.py

--- code/splitter/BootStrapping.py
+++ code/splitter/BootStrapping.py
@@ -32,8 +32,16 @@ class BootStrapping(Splitter):
 # # 自助法示例用法
 # if __name__ == '__main__':
 #     import pandas as pd
-#     data = pd.read_csv('../data/Iris.csv')
-#     print(data.head(10))
-#     data_split = BootStrapping(data)
+#     # 鸢尾花数据集
+#     iris = pd.read_csv('../data/Iris.csv')
+#     print(iris.head(10))
+#     data_split = BootStrapping(iris)
 #     train_data, test_data = data_split.split()
 #     print(train_data, test_data)
+#     # 红酒数据集
+#     wine = pd.read_csv('../data/WineQT.csv')
+#     print(wine.head(10))
+#     data_split = BootStrapping(wine)
+#     train_data, test_data = data_split.split()
+#     print(train_data, test_data)
+
