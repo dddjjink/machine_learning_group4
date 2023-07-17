bab7c46155120c6500fde982cb3804febd3a6b10
Hydrogen-577 <97822083+Hydrogen-577@users.noreply.github.com>
2023-07-17 15:06:31 +0800

    Update HoldOut.py

--- code/splitter/HoldOut.py
+++ code/splitter/HoldOut.py
@@ -1,32 +1,38 @@
+from Splitter import Splitter
 import random as r
-import Splitter
 
 
 # 留出法
 class HoldOut(Splitter):
-    def train_test_split(data, test_size):
-        """
-        将数据集划分为训练集和测试集
-        :param data: 原始数据集
-        :param test_size: 测试集所占比例
-        :return: 训练集和测试集
-        """
+    def __init__(self, dataset, test_size=0.2):
+        super().__init__(dataset)
+        self.split_size = test_size
 
+    def __call__(self, *args, **kwargs):
+        self.split()
+
+    def split(self):
         # 计算测试集的数量
-        test_num = int(len(data) * test_size)
+        test_num = int(len(self.data) * self.split_size)
 
         # 打乱数据集
-        r.shuffle(data)
+        '''
+        !!!!!!!!!!此处有误!!!!!!!!!!
+        '''
+        r.shuffle(self.data)
+
         # 划分数据集
-        train_data = data[:-test_num]
-        test_data = data[-test_num:]
+        train_data = self.data[:-test_num]
+        test_data = self.data[-test_num:]
 
         return train_data, test_data
 
 
 # # 留出法示例用法
-# data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# test_size = 0.2
-# train_data, test_data = train_test_split(data, test_size)
-# print("训练集:", train_data)
-# print("测试集:", test_data)
+# if __name__ == '__main__':
+#     import pandas as pd
+#     data = pd.read_csv('../data/Iris.csv')
+#     print(data.head(10))
+#     data_split = HoldOut(data)
+#     train_data, test_data = data_split.split()
+#     print(train_data, test_data)
