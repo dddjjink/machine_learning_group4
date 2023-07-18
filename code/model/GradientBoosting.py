d1a4bac45c4ef5cea41b0e6e40b225551897f131
stupid-vegetable-bird <97822083+stupid-vegetable-bird@users.noreply.github.com>
2023-07-18 22:10:11 +0800

    Update GradientBoosting.py

--- code/model/GradientBoosting.py
+++ code/model/GradientBoosting.py
@@ -1,56 +1,136 @@
-import CART_regression_tree
 import numpy as np
+from Model import Model
 
 
-def get_init_val(y):
-    return sum(y) / len(y)
-
-
-def get_residuals(y, y_hat):
-    y_residuals = []
-    for i in range(len(y)):
-        y_residuals.append(y[i] - y_hat[i])
-    return y_residuals
-
-
-def GBDT_cal_err(Y_test, predicts):
-    y_test = np.array(Y_test)
-    pre_y = np.array(predicts)
-    error = np.square(y_test - pre_y).sum() / len(Y_test)
-    return error
-
-
-class GBDT(object):
-    def __init__(self):
-        self.trees = None  # 存储生成的多棵回归树
-        self.learn_rate = None  # 学习率
-        self.init_val = None  # 初始值
-
-    def fit(self, X, Y, n_estimates, learn_rate, min_sample, min_err):
+class GBDT(Model):
+    def __init__(self, n_estimators=100, learning_rate=0.1, max_depth=3):
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
         self.trees = []
-        # 获取初始值
-        self.init_val = get_init_val(Y)
-        n = len(Y)
-        # 以样本标签的均值作为初始预测值
-        y_hat = [self.init_val] * n
-        # 生成初始残差
-        y_residuals = get_residuals(Y, y_hat)
 
-        self.learn_rate = learn_rate
-        for k in range(n_estimates):
-            # 拟合残差生成CART树
-            tree = CART_regression_tree.CART_regression(X, y_residuals, min_sample, min_err)
-            for i in range(len(X)):
-                res_hat = CART_regression_tree.predict(X[i], tree)
-                y_hat[i] += self.learn_rate * res_hat
-            y_residuals = get_residuals(Y, y_hat)
+    def fit(self, X, y):
+        self.trees = []
+        # 初始化预测值
+        predictions = np.zeros(len(y))
+
+        for _ in range(self.n_estimators):
+            # 计算残差
+            residual = y - predictions
+            # 训练一个决策树
+            tree = self._build_tree(X, residual, depth=0)
             self.trees.append(tree)
+            # 更新预测值
+            predictions += self.learning_rate * self.predict_tree(X, tree)
 
-    def GBDT_predict(self, X_test):
-        predicts = []
-        for i in range(len(X_test)):
-            pre_y = self.init_val
-            for tree in self.trees:
-                pre_y += CART_regression_tree.predict(X_test[i], tree)
-            predicts.append(pre_y)
-        return predicts
+    def predict(self, X):
+        predictions = np.zeros(len(X))
+        for tree in self.trees:
+            predictions += self.learning_rate * self.predict_tree(X, tree)
+        return predictions
+
+    def _build_tree(self, X, y, depth):
+        if depth >= self.max_depth or len(set(y)) == 1:
+            return np.mean(y)
+
+        best_split_feature, best_split_value, best_gain = None, None, -np.inf
+        best_left_indices, best_right_indices = None, None
+
+        for feature in range(X.shape[1]):
+            unique_values = np.unique(X[:, feature])
+            for value in unique_values:
+                left_indices = np.where(X[:, feature] <= value)[0]
+                right_indices = np.where(X[:, feature] > value)[0]
+                gain = self.calculate_gain(y, y[left_indices], y[right_indices])
+                if gain > best_gain:
+                    best_gain = gain
+                    best_split_feature = feature
+                    best_split_value = value
+                    best_left_indices = left_indices
+                    best_right_indices = right_indices
+
+        if best_gain == 0:
+            return np.mean(y)
+
+        left_tree = self._build_tree(X[best_left_indices], y[best_left_indices], depth + 1)
+        right_tree = self._build_tree(X[best_right_indices], y[best_right_indices], depth + 1)
+        return {'feature': best_split_feature, 'value': best_split_value, 'left': left_tree, 'right': right_tree}
+
+    def predict_tree(self, X, tree):
+        if isinstance(tree, np.float64):
+            return np.full(len(X), tree)
+        predictions = np.zeros(len(X))
+        for i in range(len(X)):
+            if X[i, tree['feature']] <= tree['value']:
+                predictions[i] = self.predict_tree(X[i].reshape(1, -1), tree['left'])
+            else:
+                predictions[i] = self.predict_tree(X[i].reshape(1, -1), tree['right'])
+        return predictions
+
+    def calculate_gain(self, parent, left, right):
+        return self.calculate_mse(parent) - (len(left) / len(parent)) * self.calculate_mse(left) \
+            - (len(right) / len(parent)) * self.calculate_mse(right)
+
+    def calculate_mse(self, labels):
+        if len(labels) == 0:
+            return 0
+        mean_value = np.mean(labels)
+        mse = np.mean((labels - mean_value) ** 2)
+        return mse
+
+
+# # GDBT示例用法
+# if __name__ == '__main__':
+#     import pandas as pd
+#     from sklearn.model_selection import train_test_split
+# 
+#     # '''
+#     # 对本例目前使用的鸢尾花数据集不适用，若要使用该数据集，需要先对target数据进行处理（str->int）
+#     # '''
+#     # # 鸢尾花数据集
+#     # # 数据载入
+#     # iris = pd.read_csv('../data/Iris.csv')
+#     # # print(iris.head(10))
+#     # # 数据分割
+#     # x = iris.drop(['Species', 'Id'], axis=1).values
+#     # y = iris['Species'].values
+#     # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # # print(x_train, x_test, y_train, y_test)
+#     # # 模型训练、预测
+#     # clf = GBDT()
+#     # clf.fit(x_train, y_train)
+#     # train_predict = clf.predict(x_train)
+#     # test_predict = clf.predict(x_test)
+#     # print(test_predict)
+# 
+#     # 红酒数据集
+#     # 数据载入
+#     wine = pd.read_csv('../data/WineQT.csv')
+#     # print(wine.head(10))
+#     # 数据分割
+#     x = wine.drop(['quality', 'Id'], axis=1).values
+#     y = wine['quality'].values
+#     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # print(x_train, x_test, y_train, y_test)
+#     # 模型训练、预测
+#     clf = GBDT()
+#     clf.fit(x_train, y_train)
+#     train_predict = clf.predict(x_train)
+#     test_predict = clf.predict(x_test)
+#     print(train_predict)
+# 
+#     # 心脏病数据集
+#     # 数据载入
+#     heart = pd.read_csv('../data/heart.csv')
+#     # print(heart.head(10))
+#     # 数据分割
+#     x = heart.drop(['target'], axis=1).values
+#     y = heart['target'].values
+#     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # print(x_train, x_test, y_train, y_test)
+#     # 模型训练、预测
+#     clf = GBDT()
+#     clf.fit(x_train, y_train)
+#     train_predict = clf.predict(x_train)
+#     test_predict = clf.predict(x_test)
+#     print(test_predict)
