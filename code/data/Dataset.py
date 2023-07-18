bc3728733ae38e1f2b41e9b9319eed52485776df
stupid-vegetable-bird <97822083+stupid-vegetable-bird@users.noreply.github.com>
2023-07-18 12:55:54 +0800

    Update Dataset.py

--- code/data/Dataset.py
+++ code/data/Dataset.py
@@ -4,11 +4,18 @@ import pandas as pd
 class Dataset:
     def __init__(self, path) -> None:
         self.path = path
-        self.data: pd.DataFrame = None
+        self.dataset: pd.DataFrame = None
+        self.data = None
+        self.target = None
 
     # 使用某个数据集时再加载
     def load(self):
-        self.data = pd.read_csv(self.path)
+        self.dataset = pd.read_csv(self.path)
+
+    # 对数据集分x和y
+    def data_target_split(self, x, y):
+        self.data = x
+        self.target = y
 
 
 # 鸢尾花数据集
@@ -16,11 +23,23 @@ class IrisDataset(Dataset):
     def __init__(self):
         super().__init__('Iris.csv')
 
+    def data_target(self):
+        super().load()
+        x = self.dataset[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+        y = self.dataset['Species'].values
+        super().data_target_split(x, y)
+
 
 # 红酒数据集
 class WineQualityDataset(Dataset):
     def __init__(self):
         super().__init__('WineQT.csv')
+
+    def data_target(self):
+        super().load()
+        x = self.dataset.drop(['quality', 'Id'], axis=1).values
+        y = self.dataset['quality'].values
+        super().data_target_split(x, y)
 
 
 # 心脏病数据集，二分类数据集，适用逻辑回归、SVM、决策树
@@ -28,15 +47,21 @@ class HeartDiseaseDataSet(Dataset):
     def __init__(self):
         super().__init__('heart.csv')
 
+    def data_target(self):
+        super().load()
+        x = self.dataset.drop(['target'], axis=1).values
+        y = self.dataset['target'].values
+        super().data_target_split(x, y)
+
 
 # # 设定数据集对象
 # if __name__ == '__main__':
 #     iris = IrisDataset()
-#     iris.load()
-#     print(iris.data.iloc[149])
+#     iris.data_target()
+#     print(iris.data, )
 #     wine = WineQualityDataset()
-#     wine.load()
-#     print(wine.data)
+#     wine.data_target()
+#     print(wine.target)
 #     heart = HeartDiseaseDataSet()
-#     heart.load()
+#     heart.data_target()
 #     print(heart.data)
