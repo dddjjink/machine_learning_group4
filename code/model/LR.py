5c417219b75526741bd55d4ac55373f653999315
stupid-vegetable-bird <97822083+stupid-vegetable-bird@users.noreply.github.com>
2023-07-19 11:20:17 +0800

    Update LR.py

--- code/model/LR.py
+++ code/model/LR.py
@@ -1,6 +1,8 @@
 import numpy as np
+from Model import Model
 
-class LinearRegression:
+
+class LinearRegression(Model):
     def __init__(self):
         """初始化Linear Regression模型"""
         # 系数向量（θ1,θ2,.....θn）
@@ -34,3 +36,59 @@ class LinearRegression:
         X_b = np.hstack([np.ones((len(X_predict), 1)), X_predict])
         return X_b.dot(self._theta)
 
+
+# # LinearRegression示例用法
+# if __name__ == '__main__':
+#     import pandas as pd
+#     from sklearn.model_selection import train_test_split
+# 
+#     # '''
+#     # 对本例目前使用的鸢尾花数据集不适用，若要使用该数据集，需要先对target数据进行处理（str->int）
+#     # '''
+#     # # 鸢尾花数据集
+#     # # 数据载入
+#     # iris = pd.read_csv('../data/Iris.csv')
+#     # # print(iris.head(10))
+#     # # 数据分割
+#     # x = iris.drop(['Species', 'Id'], axis=1).values
+#     # y = iris['Species'].values
+#     # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # # print(x_train, x_test, y_train, y_test)
+#     # # 模型训练、预测
+#     # clf = LinearRegression()
+#     # clf.fit(x_train, y_train)
+#     # train_predict = clf.predict(x_train)
+#     # test_predict = clf.predict(x_test)
+#     # print(test_predict)
+# 
+#     # 红酒数据集
+#     # 数据载入
+#     wine = pd.read_csv('../data/WineQT.csv')
+#     # print(wine.head(10))
+#     # 数据分割
+#     x = wine.drop(['quality', 'Id'], axis=1).values
+#     y = wine['quality'].values
+#     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # print(x_train, x_test, y_train, y_test)
+#     # 模型训练、预测
+#     clf = LinearRegression()
+#     clf.fit(x_train, y_train)
+#     train_predict = clf.predict(x_train)
+#     test_predict = clf.predict(x_test)
+#     print(train_predict)
+# 
+#     # 心脏病数据集
+#     # 数据载入
+#     heart = pd.read_csv('../data/heart.csv')
+#     # print(heart.head(10))
+#     # 数据分割
+#     x = heart.drop(['target'], axis=1).values
+#     y = heart['target'].values
+#     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # print(x_train, x_test, y_train, y_test)
+#     # 模型训练、预测
+#     clf = LinearRegression()
+#     clf.fit(x_train, y_train)
+#     train_predict = clf.predict(x_train)
+#     test_predict = clf.predict(x_test)
+#     print(test_predict)
