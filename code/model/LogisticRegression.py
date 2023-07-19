baa3b3f30bfeb24342ab2b61e5043ad25b6edc33
stupid-vegetable-bird <97822083+stupid-vegetable-bird@users.noreply.github.com>
2023-07-19 11:52:17 +0800

    Update LogisticRegression.py

--- code/model/LogisticRegression.py
+++ code/model/LogisticRegression.py
@@ -1,7 +1,9 @@
 import numpy as np
+from Model import Model
 
 
-class LogisticRegression:
+# 逻辑回归模型，二分类
+class LogisticRegression(Model):
     def __init__(self, learning_rate=0.01, num_iterations=1000):
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
@@ -31,5 +33,65 @@ class LogisticRegression:
         y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
         return y_predicted_cls
 
-    def sigmoid(self,z):
+    def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
+
+
+# # LogisticRegression示例用法
+# if __name__ == '__main__':
+#     import pandas as pd
+#     from sklearn.model_selection import train_test_split
+# 
+#     # '''
+#     # 对本例的鸢尾花数据集不适用，LogisticRegression适用二分类问题
+#     # '''
+#     # # 鸢尾花数据集
+#     # # 数据载入
+#     # iris = pd.read_csv('../data/Iris.csv')
+#     # # print(iris.head(10))
+#     # # 数据分割
+#     # x = iris.drop(['Species', 'Id'], axis=1).values
+#     # y = iris['Species'].values
+#     # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # # print(x_train, x_test, y_train, y_test)
+#     # # 模型训练、预测
+#     # clf = LogisticRegression()
+#     # clf.fit(x_train, y_train)
+#     # train_predict = clf.predict(x_train)
+#     # test_predict = clf.predict(x_test)
+#     # print(test_predict)
+# 
+#     # '''
+#     # 对本例的红酒数据集不适用，LogisticRegression适用二分类问题
+#     # '''
+#     # # 红酒数据集
+#     # # 数据载入
+#     # wine = pd.read_csv('../data/WineQT.csv')
+#     # # print(wine.head(10))
+#     # # 数据分割
+#     # x = wine.drop(['quality', 'Id'], axis=1).values
+#     # y = wine['quality'].values
+#     # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # # print(x_train, x_test, y_train, y_test)
+#     # # 模型训练、预测
+#     # clf = LogisticRegression()
+#     # clf.fit(x_train, y_train)
+#     # train_predict = clf.predict(x_train)
+#     # test_predict = clf.predict(x_test)
+#     # print(train_predict)
+# 
+#     # 心脏病数据集
+#     # 数据载入
+#     heart = pd.read_csv('../data/heart.csv')
+#     # print(heart.head(10))
+#     # 数据分割
+#     x = heart.drop(['target'], axis=1).values
+#     y = heart['target'].values
+#     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=66)
+#     # print(x_train, x_test, y_train, y_test)
+#     # 模型训练、预测
+#     clf = LogisticRegression()
+#     clf.fit(x_train, y_train)
+#     train_predict = clf.predict(x_train)
+#     test_predict = clf.predict(x_test)
+#     print(test_predict)
