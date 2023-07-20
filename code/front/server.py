50c3d4d5c13ffb282f2ade2ad14d193b528365db
WuQian1005 <139114855+WuQian1005@users.noreply.github.com>
2023-07-21 02:13:11 +0800

    Update server.py

--- code/front/server.py
+++ code/front/server.py
@@ -5,7 +5,6 @@ import websockets
 import json
 from factory.Factory import *
 
-
 # path='http://127.0.0.1:8080'
 # 处理连接建立
 
@@ -14,6 +13,7 @@ async def handle_message(websocket, path):
     async for message in websocket:
         try:
             while True:
+
                 if message == 'close_signal':
                     print("网页已关闭，连接已断开，进程终止")
                     # 收到关闭信号，断开连接并关闭程序
@@ -25,11 +25,15 @@ async def handle_message(websocket, path):
                 #text += message
                 #await websocket.send(message)
                 message = eval(message)  # 加这一行，把str类型的message转换为dict类型
-                dataset = message[list(message.keys())[0]]
-                splitter = message[list(message.keys())[1]]
-                model = message[list(message.keys())[2]]
-                evaluation = message[list(message.keys())[3]]
-                # percent = message[list(message.keys())[4]]      # 报错：list index out of range
+                dataset = message.get("dataset")
+                splitter = message.get("splitter")
+                model = message.get("model")
+                evaluation = message.get("evaluation")
+                percent=message.get("percent")
+
+                if dataset==None or splitter==None or model==None or evaluation==None or(splitter=="houldout" and percent==None):
+                     await websocket.send("data_loss")
+
                 dataset_factory = DataFactory()
                 _dataset_ = dataset_factory.create_dataset(dataset)
                 _dataset_.data_target()
@@ -38,7 +42,8 @@ async def handle_message(websocket, path):
                 y = _dataset_.target
                 # print(X)
                 splitter_factory = SplitterFactory()
-                _splitter_ = splitter_factory.create_splitter(splitter, X, y)
+                _splitter_ = splitter_factory.create_splitter(splitter, X, y,percent)
+
                 X_train, X_test, y_train, y_test = _splitter_.split()
                 # print(X_train)
                 model_factory = ModelFactory()
@@ -49,12 +54,14 @@ async def handle_message(websocket, path):
                 # print(y_test)
                 evaluation_factory = EvaluationFactory()
                 _evaluation_ = evaluation_factory.create_evaluation(evaluation, y_test, test_predict)
-
                 result = _evaluation_()
-                result_string=str(result)
-                message["result"]=result_string
-                print(message)
-                await websocket.send(str(message))
+                if result=="curve":
+                    await websocket.send(message)
+                else:
+                    result_string=str(result)
+                    message["result"]=result_string
+                    print(f"接收到的消息：{message}")
+                    await websocket.send(str(message))
                 #message['result'] = result
                 #print(message)
                 #text += '\n' + str(message)
@@ -70,7 +77,6 @@ async def handle_message(websocket, path):
 
             print("客户端已断开连接")
 
-
 def run():
     start_server = websockets.serve(handle_message, "localhost", 8082)
     # 启动服务器
@@ -80,3 +86,4 @@ def run():
     webbrowser.open(path)
     # 运行事件循环
     asyncio.get_event_loop().run_forever()
+run()
